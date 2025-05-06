3630e7bb6ced2d13d8675cad71d9289f854c4c8a
jmynn <poldenv1975@gmail.com>
2025-05-06 14:54:29 +0400

    init empty template nextjs project using docker image

---
+++ src/components/views/Main/index.tsx
@@ -0,0 +1,8 @@
+import styles from './index.module.css';
+import { ReactNode } from 'react';
+
+const Main = (): ReactNode => {
+  return <div className={styles.main}></div>;
+};
+
+export default Main;
